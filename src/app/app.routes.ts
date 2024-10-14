2407bc3ef7d19bca1048acd90ec3d02d99107f55
shivamparksmart <shivam.anand936@gmail.com>
2024-10-14 23:35:36 +0530

    We are live

---
+++ src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { ContactComponent } from './components/contact/contact.component';
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'experience', component: ExperienceComponent },
+    { path: 'contact', component: ContactComponent }
+  ];;
